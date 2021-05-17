8e120c888314fed7849ab8585292abb9c709ebc1
Miu <miyuuu0728@gmail.com>
2021-05-17 11:15:08 -0700

    added validator

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,64 +1,64 @@
-const Product = require('../models/Products')
+const Product = require("../models/Products");
 
 exports.getProducts = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split(' ')[2].split('=')[1] === 'true'
   Product.find()
     .then((products) => {
-      res.render('shop/index', {
-        pageTitle: 'Shop Page',
-        path: '/',
+      res.render("shop/index", {
+        pageTitle: "Shop Page",
+        path: "/",
         products: products,
-        isAuth: req.session.isLoggedIn
-      })
+      });
     })
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.getOneProductById = (req, res, next) => {
   Product.findById(req.params.id)
     .then((product) => {
-      res.render('shop/product-detail', {
+      res.render("shop/product-detail", {
         pageTitle: product.title,
-        path: '/products',
+        path: "/products",
         product: product,
-        isAuth: req.session.isLoggedIn
-      })
+      });
     })
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.getCart = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .execPopulate()
     .then((user) => {
-      const products = user.cart.items
-      res.render('shop/cart', {
-        pageTitle: 'Your Cart',
-        path: '/cart',
+      const products = user.cart.items;
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
         products: products,
-        isAuth: req.session.isLoggedIn
-      })
+      });
     })
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId
+  const prodId = req.body.productId;
 
   Product.findById(prodId)
     .then((product) => {
-      return req.user.addToCart(product)
+      return req.user.addToCart(product);
     })
     .then(() => {
-        res.redirect('/')
+      res.redirect("/");
     })
-    .catch((err) => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
-exports.postCartDeleteProduct = (req,res,next) => {
-    const prodId = req.body.productId
-    req.user.removeFromCart(prodId).then(() => {
-        res.redirect('/cart')
-    }).catch(err => console.log(err))
-}
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .removeFromCart(prodId)
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,32 +1,32 @@
-const express = require('express');
-// const productsController = require('../controllers/product')
-const shopController = require('../controllers/shop')
+const express = require("express");
+const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // @route   GET /
 // @desc    Get all products
 // @access  Public
-router.get('/', shopController.getProducts);
+router.get("/", shopController.getProducts);
 
 // @route   GET /products
 // @desc    Get a certain product by id
 // @access  Public
-router.get('/products/:id', shopController.getOneProductById)
+router.get("/products/:id", shopController.getOneProductById);
 
 // @route   GET /cart
 // @desc    Get products added in cart by a user
-// @access  Public
-router.get('/cart', shopController.getCart)
+// @access  Private
+router.get("/cart", isAuth, shopController.getCart);
 
 // @route   POST /cart
 // @desc    For users to add a product in a cart
-// @access  Public
-router.post('/cart', shopController.postCart)
+// @access  Private
+router.post("/cart", isAuth, shopController.postCart);
 
 // @route   POST /cart
 // @desc    Removes a product from the cart
-// @access  Public
-router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+// @access  Private
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
 module.exports = router;
